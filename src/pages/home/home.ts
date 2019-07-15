28b6ebccaad4bb4c512fab035e0b0955920ca178
Ala BEN KHLIFA <ala.benkhlifa@esprit.tn>
2019-07-15 15:48:10 +0100

    added labels and list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
-import { NavController } from 'ionic-angular';
-import { Component, OnInit } from '@angular/core';
-import { MbscEventcalendarOptions } from '@mobiscroll/angular';
-import { HttpClient } from '@angular/common/http';
+import {NavController} from 'ionic-angular';
+import {Component} from '@angular/core';
+import {MbscEventcalendarOptions} from '@mobiscroll/angular';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -9,26 +9,39 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
 
+  events: any;
+  eventSettings: MbscEventcalendarOptions = {
+    lang: 'fr',
+    theme: 'material',
+    display: 'inline',
+    view: {
+      calendar: {
+        labels: true,
+        popover: false
+      },
+      eventList: { type: 'day' }
+    }
+  };
+
+  dailySettings: MbscEventcalendarOptions = {
+    display: 'inline',
+    view: {
+      eventList: { type: 'day' }
+    }
+  };
+
   constructor(public navCtrl: NavController, private http: HttpClient) {
 
   }
-  events: any;
-
-  eventSettings: MbscEventcalendarOptions = {
-    lang: 'eng',
-    theme: 'ios',
-    display: 'inline',
-    calendarHeight: 614,
-    view: {
-      calendar: {
-        labels: true
-      }
-    }
-  };
 
   ngOnInit() {
     this.http.jsonp('https://trial.mobiscroll.com/events/', 'callback').subscribe((resp: any) => {
       this.events = resp;
     });
+  }
+
+  eventClick(event) {
+    console.log('eventclick');
+    console.log(event);
   }
 }
