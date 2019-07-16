2e6e5fcac3b711686642b53cc4acd0293dff528e
Ala BEN KHLIFA <ala.benkhlifa@esprit.tn>
2019-07-16 16:54:53 +0100

    added colors + edit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,126 +1,160 @@
-import {NavController} from 'ionic-angular';
-import {Component} from '@angular/core';
-import {MbscEventcalendarOptions, MbscPopupOptions, MbscRangeOptions, mobiscroll} from '@mobiscroll/angular';
-import {HttpClient} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ViewChild, OnInit } from '@angular/core';
+import { mobiscroll, MbscEventcalendarOptions, MbscRangeOptions, MbscPopupOptions, MbscColorOptions, MbscEventcalendar, MbscPopup } from '@mobiscroll/angular';
+import { HttpClient } from '@angular/common/http';
 
-const now = new Date();
-let id = 5;
-let eventToEdit;
+mobiscroll.settings = {
+  lang: 'fr',
+  theme: 'material'
+};
+
+let preventSet,
+  eventToEdit;
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: './home.html'
 })
-export class HomePage {
-  constructor(public navCtrl: NavController, private http: HttpClient) {  }
+export class HomePage implements OnInit {
+  constructor(private http: HttpClient) {}
 
-  eventDate = [now, new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 2)];
-  events: any;
-  allDay = false;
-  eventText = '';
-  eventDesc = '';
-
-  isFree = 'busy';
-
-  control: Array < string > ;
-  wheels: string;
-
-  btn = '<button class="mbsc-btn mbsc-btn-outline mbsc-btn-primary md-edit-btn">Edit</button>';
+  events: Array < any > ;
+  eventText: string;
+  eventDesc: string;
+  allDay: boolean;
   eventStart: Date;
   eventEnd: Date;
+  isFree: string;
   eventColor: string;
+  btn = '<button class="mbsc-btn mbsc-btn-outline mbsc-btn-primary md-edit-btn">Edit</button>';
+  controlType: Array < string > ;
+  wheelType: string;
 
-  change() {
-    this.control = this.allDay ? ['date'] : ['date', 'time'];
-    this.wheels = this.allDay ? 'MM dd yy' : '|D M d|';
-  }
+  @ViewChild('mbscCal')
+  cal: MbscEventcalendar;
+
+  @ViewChild('mbscList')
+  list: MbscEventcalendar;
+
+  @ViewChild('mbscPopup')
+  popup: MbscPopup;
 
   rangeSettings: MbscRangeOptions = {
     controls: ['date', 'time'],
     dateWheels: '|D M d|',
-    startInput: '#startDate',
-    endInput: '#endDate',
-    lang:'fr',
     tabs: false,
     responsive: {
       large: {
         touchUi: false
       }
+    }
+  };
+
+  listSettings: MbscEventcalendarOptions = {
+    display: 'inline',
+    view: {
+      eventList: { type: 'day' }
     },
-    showSelector: false
+    onPageChange: (event, inst) => {
+      const day = event.firstDay;
+      preventSet = true;
+      this.navigate(this.cal.instance, day);
+    },
+    onEventSelect: (event, inst) => {
+      if (event.domEvent.target.classList.contains('md-edit-btn')) {
+        eventToEdit = event.event;
+        this.eventToBeEdited = event.event;
+        this.updatePopup();
+        this.popup.instance.show();
+      }
+    }
+  };
+
+  calSettings: MbscEventcalendarOptions = {
+    display: 'inline',
+    view: {
+      calendar: { type: 'month' }
+    },
+    onSetDate: (event, inst) => {
+      if (!preventSet) {
+        const day = event.date;
+        this.navigate(this.list.instance, day);
+      }
+      preventSet = false;
+    }
+  };
+
+  colorSettings: MbscColorOptions = {
+    display: 'inline',
+    clear: false,
+    data: ['#fff568', '#ffc400', '#ff5252', '#f48fb1', '#ba68c8', '#8c9eff', '#90caf9', '#9ccc65', '#0db057', '#bcaaa4']
   };
 
   popupSettings: MbscPopupOptions = {
     display: 'center',
-    cssClass: 'mbsc-no-padding',
-    buttons: [{
-      text: 'Ajouter',
-      handler: 'set'
-    },
-      'cancel'
+    buttons: [
+      'cancel',
+      {
+        text: 'Save',
+        handler: 'set'
+      }
     ],
-
-
-    headerText: 'Ajouter une location',
+    headerText: 'Edit event',
     onSet: (event, inst) => {
-      this.events.push({
-        id: id,
-        start: this.eventDate[0],
-        end: this.eventDate[1],
-        text: (this.eventText || 'New Event'),
-        title: this.eventText || 'New Event',
-        description: this.eventDesc,
+      this.saveChanges();
+    }
+  };
+
+  navigate(inst, val) {
+    if (inst) {
+      inst.navigate(val);
+    }
+  }
+
+  change() {
+    this.controlType = this.allDay ? ['date'] : ['date', 'time'];
+    this.wheelType = this.allDay ? 'MM dd yy' : '|D M d|';
+  }
+
+  eventToBeEdited: any;
+
+  saveChanges() {
+    const eventToSave = {
+        id: eventToEdit.id,
+        text: this.eventText + this.btn,
+        desc: this.eventDesc,
+        color: this.eventColor,
         allDay: this.allDay,
+        start: this.eventStart,
+        end: this.eventEnd,
         free: this.isFree === 'free'
-      });
-      this.eventText = '';
-      this.eventDesc = '';
-      id += 1;
-    }
-  };
-
-  eventSettings: MbscEventcalendarOptions = {
-    lang: 'fr',
-    theme: 'material',
-    display: 'inline',
-    view: {
-      calendar: {
-        labels: true,
-        popover: false
       },
-      eventList: {type: 'day'}
+      index = this.events.indexOf(this.events.filter(x => x.id === eventToEdit.id)[0]);
+    this.events[index] = eventToSave;
+    let startDate = new Date(this.eventToBeEdited.start).getTime();
+    let startDate2 = eventToSave.start.getTime();
+    let endDate = new Date(this.eventToBeEdited.end).getTime();
+    let endDate2 = eventToSave.end.getTime();
+    let datetime;
+    for (datetime = startDate; datetime < endDate; datetime += 86400000) {
+      let loopDay = new Date(datetime).getDate();
+      this.markedDays.forEach((markedDay) => {
+        if(markedDay.d.getDate() == loopDay
+          && markedDay.d.getMonth() == new Date(datetime).getMonth()
+          && markedDay.d.getFullYear() == new Date(datetime).getFullYear()
+          && markedDay.color == this.eventToBeEdited.color) {
+          console.log(markedDay.d);
+          this.markedDays.splice(this.markedDays.indexOf(markedDay),1);
+        }
+      });
     }
-  };
-
-  dailySettings: MbscEventcalendarOptions = {
-    display: 'inline',
-    view: {
-      eventList: {type: 'day'}
+    console.log('daysToColor');
+    for (let datetime2 = startDate2; datetime2 < endDate2; datetime2 += 86400000) {
+      console.log(new Date(datetime2));
+      this.markedDays.push({d: new Date(datetime2), color: eventToSave.color})
     }
-  };
-
-
-
-  ngOnInit() {
-    this.http.jsonp('https://trial.mobiscroll.com/events/', 'callback').subscribe((resp: any) => {
-      this.events = resp;
-    });
   }
 
-  eventClick(event) {
-    console.log('eventclick');
-    console.log(event);
-  }
-
-  showInfoSnackBar() {
-    mobiscroll.snackbar({
-      message: 'Add button clicked!',
-      color: 'success'
-    });
-  }
-
-
-  // update
   updatePopup() {
     const event = eventToEdit,
       free = event.free ? 'free' : 'busy',
@@ -144,19 +178,20 @@ export class HomePage {
     this.change();
   }
 
-  saveChanges() {
-    const eventToSave = {
-        id: eventToEdit.id,
-        text: this.eventText + this.btn,
-        desc: this.eventDesc,
-        color: this.eventColor,
-        allDay: this.allDay,
-        start: this.eventStart,
-        end: this.eventEnd,
-        free: this.isFree === 'free'
-      },
-      index = this.events.indexOf(this.events.filter(x => x.id === eventToEdit.id)[0]);
-
-    this.events[index] = eventToSave;
+  ngOnInit() {
+    this.http.jsonp('https://trial.mobiscroll.com/events-update/', 'callback').subscribe((resp: any) => {
+      for (let i = 0; i < resp.length; ++i) {
+        resp[i].text += this.btn;
+        let day;
+        let startDay = new Date(resp[i].start).getTime();
+        let endDay = new Date(resp[i].end).getTime();
+        for (day = startDay; day < endDay; day += 86400000) {
+          let loopDay=new Date(day);
+          this.markedDays.push({d: loopDay,color: resp[i].color})
+        }
+      }
+      this.events = resp;
+    });
   }
+  markedDays: Array < any > = [];
 }
